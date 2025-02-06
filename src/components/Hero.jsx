26421dea53565929b4e9a817be301ac975ebb9e0
Saidur289 <saidur.riaz1@gmailcomm>
2025-02-06 17:42:58 +0600

    create navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import img from '../assets/Riaz-final-removebg-preview.png'
+
+const Hero = () => {
+    return (
+        <div className='bg-black text-white text-center py-16'>
+            <img src={img} alt="profile"  className='mx-auto mb-8 w-48 h-48 rounded-full object-cover transform transition-transform duration-300 hover:scale-105'/>
+            <h1 className='text-4xl font-bold'>I am <span className='text-transparent bg-clip-text bg-gradient-to-t from-green-400 to-blue-400'>Front End Developer</span></h1>
+            <p className='mt-4 text-lg text-gray-300'>I specialize in building modern and responsive application</p>
+            <div className='mt-8 space-x-4'>
+                <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline transform transition-transform duration-300 px-4 py-2 rounded-full'>Connect With Me</button>
+                <button className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white hidden md:inline transform transition-transform duration-300 px-4 py-2 rounded-full'>Resume</button>
+
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
